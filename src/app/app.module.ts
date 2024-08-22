14c85d7a8b30bf526fc456aac45d09418f6eda5a
Mujahid Iqbal <mujahid.iqbal@lhr.onclout.com>
2024-08-22 12:02:46 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from '@core/helpers/auth-interceptor';
@@ -11,8 +11,12 @@ import { UnderconstructionPage } from '@core/ui/pages/underconstruction/undercon
 
 @NgModule({
   declarations: [AppComponent, UnderconstructionPage],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule],
-  providers: [HttpClient, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  providers: [
+    HttpClient,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
 })
 export class AppModule {}
